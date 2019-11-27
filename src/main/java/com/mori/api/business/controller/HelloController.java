baa27c42405c1176ac9399b1eacde16c70ec0a74
442996643@qq.com <442996643@qq.com>
2019-11-27 21:50:23 +0800

    包结构  项目

--- src/main/java/com/mori/api/business/controller/HelloController.java
+++ src/main/java/com/mori/api/business/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.mori.api.business.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.google.common.collect.ImmutableMap;
+import com.mori.dubbo.business.xcx.IHelloService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,12 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @Reference(url = "dubbo://127.0.0.1:20880")
+    @Reference(version = "1.0",timeout = 6000)
     private IHelloService iHelloService;
 
     @RequestMapping("/hello")
     public ResponseEntity<Object> hello(){
-
+        iHelloService.doHello();
         return ResponseEntity.ok(ImmutableMap.
                 builder().put("msg","ok").put("code",200).build());
     }
--- src/main/java/com/mori/api/core/listener/ApplicationEnvironmentPreparedEventListener.java
+++ src/main/java/com/mori/api/core/listener/ApplicationEnvironmentPreparedEventListener.java
@@ -1,7 +1,10 @@
 package com.mori.api.core.listener;
 
+import com.alibaba.dubbo.common.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.env.OriginTrackedMapPropertySource;
+import org.springframework.boot.origin.OriginTrackedValue;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
@@ -9,7 +12,7 @@ import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.SystemEnvironmentPropertySource;
 import org.springframework.stereotype.Component;
 
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * 环境变量监听器 处理环境变量参数
@@ -18,21 +21,52 @@ import java.util.Iterator;
 @Component
 public class ApplicationEnvironmentPreparedEventListener
 		implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
+	private static final String APPLICATION_PROPERTIES_NAME="applicationConfig: [classpath:/application.properties]";
 
 	@Override
 	public void onApplicationEvent(ApplicationEnvironmentPreparedEvent event) {
 		ConfigurableEnvironment env = event.getEnvironment();
 		MutablePropertySources propertySources = env.getPropertySources();
+//		env.getPropertySources().
+		OriginTrackedMapPropertySource applicationProperties=null;
 		Iterator<PropertySource<?>> iterator = propertySources.iterator();
 		while(iterator.hasNext()) {
 			PropertySource<?> next = iterator.next();
-			if(next instanceof SystemEnvironmentPropertySource) {
-				SystemEnvironmentPropertySource systemEnv = (SystemEnvironmentPropertySource)next;
-				String[] propertyNames = systemEnv.getPropertyNames();
-				log.debug("打印环境变量名：" + propertyNames.toString());
+			if(next.getName().equals(APPLICATION_PROPERTIES_NAME)){
+				log.info("------读取拦截配置文件 替换环境变量-----"+next.getClass());
+				 applicationProperties=(OriginTrackedMapPropertySource)next;
+				Map<String,Object> sourceMap=applicationProperties.getSource();
+				Map<String,Object> sourceMapCopy= new HashMap<>();
+				sourceMap.forEach((k,v)->{
+					OriginTrackedValue value= (OriginTrackedValue)sourceMap.get(k) ;
+					OriginTrackedValue newvalue=OriginTrackedValue.of(this.replaceEnvConfig(value.getValue().toString()),value.getOrigin());
+					sourceMapCopy.put(k,newvalue);
+				});
+				applicationProperties=new OriginTrackedMapPropertySource(APPLICATION_PROPERTIES_NAME,sourceMapCopy);
 			}
 		}
+		env.getPropertySources().replace(APPLICATION_PROPERTIES_NAME,applicationProperties);
 		log.debug("打印环境变量：{}", env);
+	}
+
+	private String replaceEnvConfig(String value) {
+
+		String REG="^\\$\\{.{0,}[/:].{0,}\\}";
+		if(value.matches(REG)){
+			int firstKey=value.indexOf("${")+2;
+			int lastKey=value.indexOf("}")-firstKey;
+			String preValue=value.substring(0,firstKey);
+			String tailValue=value.substring(lastKey);
+			String cutValue=preValue+value.substring(firstKey).substring(0,lastKey)+tailValue;
+			int firstSpiltkey=cutValue.indexOf(":");
+			String envKey=cutValue.substring(0,firstSpiltkey);
+			String envDefault=cutValue.substring(firstSpiltkey);
+			String envValue=System.getProperty(envKey);
+
+			return StringUtils.isBlank(envValue)?envDefault:envValue;
+		}else{
+			return value;
+		}
 	}
 
 }
